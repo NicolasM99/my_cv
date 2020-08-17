6d319541364071f04d1298292e9a7b0174eda337
Nicolás Moreno Posada <nicolas@192.168.0.9>
2020-08-17 15:41:50 -0500

    Updates

---
+++ precache-manifest.05b9fc71c4fb667b4690c120d61bf66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0f82eebb6f9d388ceff1fcba175c2a",
+    "url": "/my_cv/index.html"
+  },
+  {
+    "revision": "fd31d4ff9d452ae627c6",
+    "url": "/my_cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cff75a8a438be964c37f",
+    "url": "/my_cv/static/js/2.c85acd82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_cv/static/js/2.c85acd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd31d4ff9d452ae627c6",
+    "url": "/my_cv/static/js/main.eba9d752.chunk.js"
+  },
+  {
+    "revision": "29659e2d4c3e8233a2de",
+    "url": "/my_cv/static/js/runtime-main.f0f77548.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
